b50b0a2204f360c12b1c4c3db5b723b51ca0aa74
shizzuru <bastien.mill@yahoo.fr>
2018-10-13 12:56:29 +0200

    Forgot to commit these | Added template and static file support

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
-import(
-	//"html/template"
-	"net/http"
+import (
+	"html/template"
 	"log"
+	"net/http"
 )
 
-func redirect(w http.ResponseWriter, req *http.Request){
+func redirect(w http.ResponseWriter, req *http.Request) {
 	target := "https://" + req.Host + req.URL.Path
 	if len(req.URL.RawQuery) > 0 {
 		target += "?" + req.URL.RawQuery
 	}
-	http.Redirect(w,req,target,http.StatusTemporaryRedirect)
+	http.Redirect(w, req, target, http.StatusTemporaryRedirect)
 }
 
-func main(){
-	go http.ListenAndServe(":80",http.HandlerFunc(redirect))
-	http.Handle("/",http.FileServer(http.Dir("/home/shizzuru/webapp")))
-	log.Fatal(http.ListenAndServeTLS(":443","/etc/letsencrypt/live/catbox.space/fullchain.pem","/etc/letsencrypt/live/catbox.space/privkey.pem",nil))
+func home(w http.ResponseWriter, req *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/layout.html", "templates/menu.html")
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
 
+func main() {
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", home)
+
+	go http.ListenAndServe(":80", http.HandlerFunc(redirect)) //To add
+	log.Fatal(http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/catbox.space/fullchain.pem", "/etc/letsencrypt/live/catbox.space/privkey.pem", nil))
+}
